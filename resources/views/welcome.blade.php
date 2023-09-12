9a4352ede26655265a9d34e1980a71b0aca568df
LamornaRead <lamornaread@outlook.com>
2023-09-12 09:31:20 +0100

    home page card layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,49 @@
     </div>
 
     <div class="container-main">
-        <div class="company-card">
+        <div class="container-inner">
+            <div class="company-card">
 
-            <div class="company-card-header">
-                <h3>Company</h3>
+                <div class="company-card-header">
+                    <h3>Company</h3>
+                </div>
+
+                <div class="company-card-img">
+                    <img class="company-img" src="https://picsum.photos/200/200" alt="random image">
+                </div>
+
+                <a class="company-button" href="#">View More</a>
+
             </div>
 
-            <div class="company-card-img">
-                <img class="company-img" src="https://picsum.photos/200/200" alt="random image">
+            <div class="company-card">
+
+                <div class="company-card-header">
+                    <h3>Company</h3>
+                </div>
+
+                <div class="company-card-img">
+                    <img class="company-img" src="https://picsum.photos/200/200" alt="random image">
+                </div>
+
+                <a class="company-button" href="#">View More</a>
+
             </div>
 
-            <a class="company-button" href="#">View More</a>
-            
+            <div class="company-card">
+
+                <div class="company-card-header">
+                    <h3>Company</h3>
+                </div>
+
+                <div class="company-card-img">
+                    <img class="company-img" src="https://picsum.photos/200/200" alt="random image">
+                </div>
+
+                <a class="company-button" href="#">View More</a>
+
+            </div>
         </div>
+
     </div>
 @endsection
