36684743cbef4672d6e2f3ce74290622be8ec852
LamornaRead <lamornaread@outlook.com>
2023-09-12 08:56:36 +0100

    company card styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,20 @@
     <div class="header">
         <h1>Company Management Service</h1>
     </div>
+
+    <div class="container-main">
+        <div class="company-card">
+
+            <div class="company-card-header">
+                <h3>Company</h3>
+            </div>
+
+            <div class="company-card-img">
+                <img class="company-img" src="https://picsum.photos/200/200" alt="random image">
+            </div>
+
+            <a class="company-button" href="#">View More</a>
+            
+        </div>
+    </div>
 @endsection
