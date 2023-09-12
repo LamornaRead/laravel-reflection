3ec1af332ef056bfa5777624aba27296cb797ee6
LamornaRead <lamornaread@outlook.com>
2023-09-12 11:41:36 +0100

    small styling change for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+    <div class="header">
+        <h1>Admin Login</h1>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
