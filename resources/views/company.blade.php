c8d80b6b9f902f89f899784a7ba847cab3ff53fc
LamornaRead <lamornaread@outlook.com>
2023-09-19 12:11:03 +0100

    Update company.blade.php

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,46 +1,48 @@
 <x-app>
     <x-container-main>
 
-        <div>
-            <h2 class="form-header">{{ $company->name }}</h2>
-            <div class="dash-btn-con">
-                <a class="dashboard-btn" href="/home">Back</a>
+        <div class="mt-10">
+            <x-header class="mb-6" name='{{ $company->name }}'></x-header>
+            <div class="flex justify-center mb-10">
+                <x-button class="w-56 text-base" link='/home' name='Back To Dashboard'></x-button>
             </div>
         </div>
-        <div class="file-info-con">
-            <div class="file-info-top">
-                <div>
-                    <img class="file-info-img" src="{{ $company->image }}" alt="company logo">
+        <x-company-card class="lg:w-9/12 mx-auto p-4 mb-6">
+            <div class="md:flex">
+                <div class="md:mr-4">
+                    <img class="rounded-lg" src="{{ $company->image }}" alt="company logo">
                 </div>
-                <div class="file-info-list">
+                <div class="mt-2">
                     <p><strong>Company Name :</strong> {{ $company->name }}</p>
                     <p><strong>Company Email :</strong> {{ $company->email }}</p>
                     <p><strong>Company Website :</strong> {{ $company->website }}</p>
-                    <div class="file-btn-con">
-                        <a class="file-btn" href="">edit</a>
-                        <a class="file-btn" href="">delete</a>
+                    <div>
+                        <x-button class="uppercase text-sm px-4" link='#' name='edit'></x-button>
+                        <x-button class="uppercase text-sm px-3" link='#' name='delete'></x-button>
                     </div>
                 </div>
             </div>
-        </div>
+        </x-company-card>
 
-        <div class="file-info-con">
-            <h4>Employees</h4>
-        @foreach($company->employees as $em)
-            <div class="file-info-bottom">
-                <div>
-                    <p><strong>First Name :</strong> {{ $em->first_name }}</p>
-                    <p><strong>Last Name :</strong> {{ $em->last_name }}</p>
-                    <p><strong>Email :</strong> {{ $em->email }}</p>
-                    <p><strong>Telephone :</strong> {{ $em->phone_number }}</p>
-                </div>
-                <div class="">
-                        <a class="file-btn" href="">edit</a>
-                        <a class="file-btn" href="">delete</a>
+        <x-company-card class="lg:w-9/12 mx-auto p-4">
+            <h4 class="text-3xl pb-6">Employees</h4>
+
+            @foreach($company->employees as $em)
+                <div class="lg:grid lg:grid-cols-6 bg-gray-300 p-3 rounded border border-gray-400 mb-6">
+                    <div class="col-span-3 xl:col-span-4">
+                        <p><strong>First Name :</strong> {{ $em->first_name }}</p>
+                        <p><strong>Last Name :</strong> {{ $em->last_name }}</p>
+                        <p><strong>Email :</strong> {{ $em->email }}</p>
+                        <p class="lg:mb-0"><strong>Telephone :</strong> {{ $em->phone_number }}</p>
                     </div>
-            </div>
-        @endforeach
-        </div>
+                    <div class="col-span-3 xl:col-span-2 my-auto">
+                        <x-button class="uppercase text-sm px-4 lg:mr-3" link='#' name='edit'></x-button>
+                        <x-button class="uppercase text-sm px-3" link='#' name='delete'></x-button>
+                    </div>
+                </div>
+            @endforeach
+        </x-company-card>
+
     </x-container-main>
 
 </x-app>
