1e3a0ac1e8f0c51efb820e4bf800fc2a6546d8e1
LamornaRead <lamornaread@outlook.com>
2023-09-24 17:16:08 +0100

    update form company updated

--- public/js/forms.js
+++ public/js/forms.js
@@ -115,3 +115,70 @@ function validateCompany() {
     return true;
 }
 
+function validateCompanyUpdate() {
+    let fields = [document.getElementById('name'), 
+        document.getElementById('email'),
+        // document.getElementById('image'),
+        document.getElementById('website'),
+    ];
+    let errMessage = [document.getElementById('nameErr'),
+        document.getElementById('emailErr'), 
+        document.getElementById('imageErr'), 
+        document.getElementById('websiteErr')
+    ];
+
+    var err = 0;
+    for (var i = 0; i < fields.length; i++) {
+        if(fields[i].value == "") {
+            err++
+            fields[i].style.border = '1px solid red';
+
+            if(fields[2]) {
+                fields[2].style.border = '';
+            } else {
+                fields[i].style.border = '1px solid red';
+            }
+            errMessage[i].innerHTML = `Please fill in ${fields[i].name}`;
+            errMessage[i].style.display = 'block';
+
+
+        } else {
+            fields[i].style.border = '';
+            errMessage[i].innerHTML = '';
+            errMessage[i].style.display = 'none'
+
+
+            if(!emailFormat.test(email.value)) {
+                err + 1;
+                fields[1].style.border = '1px solid red';
+                errMessage[1].style.display = 'block';
+                errMessage[1].innerHTML = `Please fill out a valid email`;
+            } else {
+                fields[1].style.border = '';
+                errMessage[1].style.display = 'none';
+                errMessage[1].innerHTML = '';
+            }
+            
+            if (fileInput.value != '') {
+                if(!allowedExtensions.exec(fileInput.value)){
+                    err + 1;
+                    errMessage[2].style.display = 'block';
+                    errMessage[2].innerHTML = 'Please add a valid filetype';
+                    // fileInput.value = '';
+                }
+            } else {
+                errMessage[2].style.display = 'none';
+                errMessage[2].innerHTML = '';
+            }
+        }
+    }
+
+    if (err === 0) {
+        console.log('form success');
+    } else {
+        console.log('form failed');
+        return false;
+    }
+
+    return true;
+}
--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -25,24 +25,27 @@
                         </x-form.field>
     
                         @endif
-                        <form method="POST" action="/companies/edit/{{ $company->id }}" enctype="multipart/form-data">
+                        <form method="POST" action="/companies/edit/{{ $company->id }}" enctype="multipart/form-data" onsubmit="return validateCompanyUpdate()">
                                 @csrf
                                 @method('PATCH')
                             
                             <x-form.field>
                                 <x-form.label for='name'>Company Name <i class="fa-solid fa-star-of-life fa-2xs" style="color: #cc0000;"></i></x-form.label>
                                 <x-form.input name='name' value='{{ $company->name }}'></x-form.input>
+                                <x-form.js-error-two id="nameErr"></x-form.js-error-two>
                             </x-form.field>
             
                             <x-form.field>
                                 <x-form.label for='email'>Company Email <i class="fa-solid fa-star-of-life fa-2xs" style="color: #cc0000;"></i></x-form.label>
                                 <x-form.input name='email' type='email' value='{{ $company->email }}'></x-form.input>
+                                <x-form.js-error-two id="emailErr"></x-form.js-error-two>
                             </x-form.field>
                             
                             <div class="sm:grid grid-cols-3">
                                 <x-form.field class="col-span-2 w-full">
                                     <x-form.label for='image'>Company Image 100x100 <i class="fa-solid fa-star-of-life fa-2xs" style="color: #cc0000;"></i></x-form.label>
                                     <x-form.file name='image'></x-form.file>
+                                    <x-form.js-error-two id="imageErr"></x-form.js-error-two>
                                 </x-form.field>
                                 <img class="rounded-lg w-32 mx-auto" src="{{ asset('storage/' . $company->image) }}" alt="company logo">
                             </div>
@@ -51,6 +54,7 @@
                             <x-form.field>
                                 <x-form.label for='website'>Company Website <i class="fa-solid fa-star-of-life fa-2xs" style="color: #cc0000;"></i></x-form.label>
                                 <x-form.input name='website' value='{{ $company->website }}'></x-form.input>
+                                <x-form.js-error-two id="websiteErr"></x-form.js-error-two>
                             </x-form.field>
             
                             <x-form.field>
