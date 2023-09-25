7364da13712ce5c23ff6004ac1e7b43cdf79642b
LamornaRead <lamornaread@outlook.com>
2023-09-25 14:48:09 +0100

    updated dash item component

--- resources/views/components/dashboard/layout.blade.php
+++ resources/views/components/dashboard/layout.blade.php
@@ -3,11 +3,11 @@
     <div class="md:grid grid-cols-8">
         <div class="col-span-1 bg-gray-900 p-4 hidden md:block">
             <ul class="liststyle-none p-0">
-                <x-dash-item href="/home" :active="request()->routeIs('home')">All Info</x-dash-item>
-                <x-dash-item href="/admin/all-companies" :active="request()->routeIs('all-companies')">All Companies</x-dash-item>
-                <x-dash-item href="/admin/all-employees" :active="request()->routeIs('all-employees')">All Employees</x-dash-item>
-                <x-dash-item href="/companies/create" :active="request()->routeIs('create-company')">Add Company</x-dash-item>
-                <x-dash-item href="/employee/create" :active="request()->routeIs('create-employee')">Add Employee</x-dash-item>
+                <x-dashboard.item href="/home" :active="request()->routeIs('home')">All Info</x-dashboard.item>
+                <x-dashboard.item href="/admin/all-companies" :active="request()->routeIs('all-companies')">All Companies</x-dashboard.item>
+                <x-dashboard.item href="/admin/all-employees" :active="request()->routeIs('all-employees')">All Employees</x-dashboard.item>
+                <x-dashboard.item href="/companies/create" :active="request()->routeIs('create-company')">Add Company</x-dashboard.item>
+                <x-dashboard.item href="/employee/create" :active="request()->routeIs('create-employee')">Add Employee</x-dashboard.item>
             </ul>
         </div>
         
