dc84d29a5a7b035ed7eb0f4ce002a475e5768b6f
LamornaRead <lamornaread@outlook.com>
2023-09-26 14:23:03 +0100

    Update all-employees.blade.php

--- resources/views/admin/all-employees.blade.php
+++ resources/views/admin/all-employees.blade.php
@@ -1,7 +1,6 @@
 <x-layout.app>
     <x-dashboard.layout>
         <x-slot name="top">
-
             <x-dropdown.box>
                 <div class="ml-auto mr-auto md:ml-0 md:mr-0">
                     <x-search.form></x-search.form>
@@ -17,12 +16,8 @@
                     @endforeach
                 </x-dropdown.company>
             </x-dropdown.box>
-
         </x-slot>
         
-
-
-
         <div>
             <h4 class="text-2xl lg:text-3xl pb-4 pt-2 lg:ml-2 mb-6 border-b border-gray-400">All Employees</h4>
         </div>
@@ -34,24 +29,21 @@
         @endif
 
         @if($employees->count())
-            <div>
-                @foreach($employees as $employee)
-                    <x-layout.card class="p-3 mb-4 lg:grid grid-cols-10 bg-gray-200">
-                        <div class="col-span-2">
-                            <p class="text-base lg:m-0 col-span-1"><strong>Name :</strong> {{ $employee->first_name }} {{ $employee->last_name }}</p>
-                        </div>
+            @foreach($employees as $employee)
+                <x-layout.card class="p-3 mb-4 lg:grid grid-cols-10 bg-gray-200">
+                    <div class="col-span-2">
+                        <p class="text-base lg:m-0 col-span-1"><strong>Name :</strong> {{ $employee->first_name }} {{ $employee->last_name }}</p>
+                    </div>
 
-                        <div class="col-span-7">
-                            <p class="text-base lg:ml-4 lg:mb-0 col-span-3"><strong>Company :</strong> {{ $employee->company->name }}</p>
-                        </div>
+                    <div class="col-span-7">
+                        <p class="text-base lg:ml-4 lg:mb-0 col-span-3"><strong>Company :</strong> {{ $employee->company->name }}</p>
+                    </div>
 
-                        <div class="col-span-1">
-                            <x-button.normal class="mt-4 px-4 py-2" href="/employee/edit/{{ $employee->id }}">Edit</x-button.normal>
-                        </div>
-                    </x-layout.card>
-                    
-                @endforeach
-            </div>
+                    <div class="col-span-1">
+                        <x-button.normal class="mt-4 px-4 py-2" href="/employee/edit/{{ $employee->id }}">Edit</x-button.normal>
+                    </div>
+                </x-layout.card>
+            @endforeach
 
             <div class="pt-8">
                 {{ $employees->links() }}
@@ -61,5 +53,4 @@
             <p class="text-xl sm:text-2xl text-center uppercase tracking-widest">Come Back Later</p>
         @endif
     </x-dashboard.layout>
-
 </x-layout.app>
