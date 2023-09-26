140466355fd1fa8d02215a5ad4196fe7c0db9e73
LamornaRead <lamornaread@outlook.com>
2023-09-26 08:48:21 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,24 +7,20 @@
         </x-slot>
 
         @if($companies->count())
-    
-        <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-5">
-        @foreach($companies as $company)
-            <x-layout.card>
-                <x-content.company :company="$company"></x-content.company>
-            </x-layout.card>
-            
+            <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-5">
+                @foreach($companies as $company)
+                    <x-layout.card>
+                        <x-content.company :company="$company"></x-content.company>
+                    </x-layout.card>
+                @endforeach
+            </div>
 
-        @endforeach
-    
-        </div>
-
-        <div class="pt-8">
-                {{ $companies->links() }}
-        </div>
+            <div class="pt-8">
+                    {{ $companies->links() }}
+            </div>
         @else
-        <p class="text-xl sm:text-2xl text-center uppercase tracking-widest mt-24 sm:mt-56">No companies have been added yet</p>
-        <p class="text-xl sm:text-2xl text-center uppercase tracking-widest">Come Back Later</p>
+            <p class="text-xl sm:text-2xl text-center uppercase tracking-widest mt-24 sm:mt-56">No companies have been added yet</p>
+            <p class="text-xl sm:text-2xl text-center uppercase tracking-widest">Come Back Later</p>
         @endif
     </x-dashboard.layout>
 </x-layout.app>
